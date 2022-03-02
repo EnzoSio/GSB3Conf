da633dc89f691a4558be4a04a5305a500f60d182
EnzoSio <76437510+EnzoSio@users.noreply.github.com>
2022-03-02 16:37:32 +0100

    MEP Formulaire Inscription

--- application/views/vue_inscription.php
+++ application/views/vue_inscription.php
@@ -6,24 +6,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Inscription</title>
     <?php
-        echo link_tag('assets/css/styles.css');
+        echo link_tag('assets/css/inscription.css');
     ?>
 
 </head>
 <body>
-	<div id="container">
-            <!-- zone de connexion -->
+    <div class="container">
+        <header class="header">
+            <h1 id="title" class="text-center">Inscription à une conférence</h1>
+            <p id="description" class="description text-center">
+            Bienvenue 
             
-            <?php
-                echo form_open('MonControleur/inscription');
-                echo heading("Inscription", 1);
-                echo form_label("Login : ");
-                echo form_input('login', set_value('login'));
-                echo form_label("Mot de Passe : ");
-                echo form_password('mdp', set_value('mdp'));                
-                echo form_submit("Valider", "Valider");
-                echo form_close();
-            ?>
-	</div>
+            <br>
+                Veuillez-remplir le formulaire ci-dessous pour vous inscrire à une conférence
+            </p>
+        </header>
+        <form id="survey-form">
+            <div class="form-group">
+                <label id="" for="horaire">Horaire</label>
+                <input type="time" id="appt" name="appt" min="09:00" max="18:00" required>
+            </div>
+            <div class="form-group">
+                <label id="" for="duree">Durée</label>
+                <input type="text" name="duree" id="duree" class="form-control" placeholder="Entrez la durée" required />
+            </div>
+            <div class="form-group">
+                <label id="" for="duree">Nombre de Places</label>
+                <input type="text" name="nbPlace" id="nbPlace" class="form-control" placeholder="Entrez le nombre de place" required />
+            </div>
+            <div class="form-group">
+                <label id="" for="duree">Durée</label>
+                <input type="text" name="duree" id="duree" class="form-control" placeholder="Entrez la durée" required />
+            </div>
+            <div class="form-group">
+                <label id="" for="duree">Date</label>
+                <input id="date" type="date" value="2017-06-01">
+            </div>
+            <div class="form-group">
+                <button type="submit" id="submit" class="submit-button">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
